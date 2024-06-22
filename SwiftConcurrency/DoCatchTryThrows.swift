313029919f659a91a5309e4c1eba66d76f6cefed
Dmitrii Eselidze <dmitry.eselidze@yandex.ru>
2024-06-22 21:47:39 +0300

    first commit

---
+++ SwiftConcurrency/DoCatchTryThrows.swift
@@ -0,0 +1,18 @@
+//
+//  DoCatchTryThrows.swift
+//  SwiftConcurrency
+//
+//  Created by Dmitrii Eselidze on 22.06.2024.
+//
+
+import SwiftUI
+
+struct DoCatchTryThrows: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DoCatchTryThrows()
+}
