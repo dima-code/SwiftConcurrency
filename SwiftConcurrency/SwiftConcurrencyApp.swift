cfb520916ecd9863286411b438b2278bce108a7d
Dmitrii Eselidze <dmitry.eselidze@yandex.ru>
2024-06-22 12:47:40 +0300

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Dmitrii Eselidze on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
